ad7a2a1dc5644f5f6f665611c2440cb7b97ce5b3
Ryo Nakadoi <r_nakadoi@corp.allabout.co.jp>
2020-01-30 18:21:16 +0900

    ヘッダー部分の実装

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="utf-8">
+    <title>Macrobi</title>
+    <link rel="stylesheet" href="app.css">
+    <link href="https://fonts.googleapis.com/css?family=Sawarabi+Gothic|Sigmar+One&display=swap" rel="stylesheet">
+</head>
+<body>
+    <header class="header">
+        <h1>Macrobi</h1>
+        <nav id="top-nav">
+        <ul>
+            <li><a href="login.php">ログイン</a></li>
+            <li><a href="signup.php">新規登録</a></li>
+        </ul>
+        </nav>
+    </header>
+
+    <main class="main">
+    <img class="top-img" src="images/salad.jpg" alt="サラダ画像">
+    </main>
+
+    <footer class="footer"></footer>
+</body>
+</html>
