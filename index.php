faecb02f1d58d65290c5080e4f01b10fd7c94f83
Ryo Nakadoi <r_nakadoi@corp.allabout.co.jp>
2020-02-04 09:56:57 +0900

    メイン部分、フッターを作成

--- index.php
+++ index.php
@@ -8,20 +8,65 @@
     <link href="https://fonts.googleapis.com/css?family=Sawarabi+Gothic|Sigmar+One&display=swap" rel="stylesheet">
 </head>
 <body>
-    <header class="header">
+    <header class="header site-width">
         <h1><a href="">Macrobi</a></h1>
         <nav id="top-nav">
         <ul>
-            <li><a href="login.php">ログイン</a></li>
-            <li><a href="signup.php">新規登録</a></li>
+            <li><a href="index.php">TOP</a></li>
+            <li><a href="#about">ABOUT</a></li>
+            <li><a href="#menu">MENU</a></li>
+            <li><a href="#contact">CONTACT</a></li>
         </ul>
         </nav>
     </header>
 
     <main class="main">
-    <img class="top-img" src="images/salad.jpg" alt="サラダ画像">
+        <img class="top-img" src="images/salad.jpg" alt="ヒーローバナー">
+        <section id="about" class="site-width">
+            <h1 class="title">ABOUT</h1>
+            <p>
+                sample text.sample text.sample text.sample text.sample text.sample text.sample text.sample text.
+                sample text.sample text.sample text.sample text.sample text.sample text.sample text.sample text.
+                sample text.sample text.sample text.sample text.sample text.sample text.sample text.sample text.
+                sample text.sample text.sample text.
+            </p>
+        </section>
+
+        <section id="menu" class="site-width">
+            <h1 class="title">MENU</h1>
+            <p>
+                sample text.sample text.sample text.sample text.sample text.sample text.sample text.sample text.
+                sample text.sample text.sample text.sample text.sample text.sample text.sample text.sample text.
+                sample text.sample text.sample text.sample text.sample text.sample text.sample text.sample text.
+                sample text.sample text.sample text.
+            </p>
+        </section>
+        <section class="site-width">
+            <h1>新規登録は<a href="signup.php">こちら</a>から</h1>
+            <h1>ログインは<a href="login.php">こちら</a>から</h1>
+        </section>
+        <section class="site-width">
+            <form method="post">
+                <h1 class="title">CONTACT</h1>
+                <table>
+                        <tr>
+                            <th>お名前</th>
+                            <td><input type="text" name="name"></td>
+                        </tr>
+                        <tr>
+                            <th>メールアドレス</th>
+                            <td><input type="text" name="email"></td>
+                        </tr>
+                        <tr>
+                            <th>お問い合わせ内容</th>
+                            <td><textarea name="comment"></textarea></td>
+                        </tr>
+                </table>
+                <input type="submit" value="送信する">
+            </form>
+        </section>
     </main>
 
-    <footer class="footer"></footer>
+    <footer class="footer">CopyRight Macrobi. All Right Reserved.</footer>
 </body>
 </html>
