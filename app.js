4ef14db260da9d03d29f0f61294fe3b267dd175c
centerRyo <abc.atrn@gmail.com>
2020-02-11 15:02:43 +0900

    Emailのバリデーション追加

--- app.js
+++ app.js
@@ -13,17 +13,32 @@ $(function() {
   const MSG_TEXTAREA_MAX = '200文字以内で入力して下さい';
 
   $('.js-valid-text').on('keyup', function() {
-    var form_g = $(this).closest('.contact__list');
+    var contact_g = $(this).closest('.contact__list');
 
     if ($(this).val().length === 0) {
-      form_g.removeClass('has-success').addClass('has-error');
-      form_g.find('.area-msg').text(MSG_REQUIRED);
+      contact_g.removeClass('has-success').addClass('has-error');
+      contact_g.find('.area-msg').text(MSG_REQUIRED);
     } else if ($(this).val().length > 20) {
-      form_g.removeClass('has-success').addClass('has-error');
-      form_g.find('.area-msg').text(MSG_TEXT_MAX);
+      contact_g.removeClass('has-success').addClass('has-error');
+      contact_g.find('.area-msg').text(MSG_TEXT_MAX);
     } else {
-      form_g.removeClass('has-error').addClass('has-success');
-      form_g.find('.area-msg').text('');
+      contact_g.removeClass('has-error').addClass('has-success');
+      contact_g.find('.area-msg').text('');
+    }
+  });
+
+  $('.js-valid-email').on('keyup', function() {
+    var contact_g = $(this).closest('.contact__list');
+
+    if ($(this).val().length === 0) {
+      contact_g.removeClass('has-success').addClass('has-error');
+      contact_g.find('.area-msg').text(MSG_REQUIRED);
+    } else if ($(this).val().length > 50 || !$(this).val().match(/^[a-zA-Z0-9.!#$%&'*+\/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/)) {
+      contact_g.removeClass('has-success').addClass('has-error');
+      contact_g.find('.area-msg').text(MSG_EMAIL_TYPE);
+    } else {
+      contact_g.removeClass('has-error').addClass('has-success');
+      contact_g.find('.area-msg').text('');
     }
   });
 });
