31e57a8219e650157b9e14ca33f7a3612eb5041f
Ryo Nakadoi <r_nakadoi@corp.allabout.co.jp>
2020-02-12 11:45:45 +0900

    ログインロジックを生成した

--- login.php
+++ login.php
@@ -2,6 +2,51 @@
   error_reporting(E_ALL);
   ini_set('display_errors', 'On');
 
+  if (!empty($_POST)) {
+      define('MSG01', '入力必須です');
+      define('MSG02', 'メールアドレスまたはパスワードが間違っています');
+
+      $err_msg = [];
+
+      if (empty($_POST['email'])) {
+          $err_msg['email'] = MSG01;
+      }
+
+      if (empty($_POST['pass'])) {
+          $err_msg['pass'] = MSG01;
+      }
+
+      if (empty($err_msg)) {
+          $email = $_POST['email'];
+          $pass = $_POST['pass'];
+
+          $dsn = 'mysql:dbname=macrobi;host=localhost;charset=utf8';
+          $user = 'root';
+          $password = 'root';
+          $options = array(
+              PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+              PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+              PDO::MYSQL_ATTR_USE_BUFFERED_QUERY => true
+          );
+
+          $dbh = new PDO($dsn, $user, $password, $options);
+          $stmt = $dbh->prepare('SELECT * FROM users WHERE email = :email AND password = :pass');
+          $stmt->execute(array(':email' => $email, ':pass' => $pass));
+
+          $result = 0;
+          $result = $stmt->fetch(PDO::FETCH_ASSOC);
+
+          if (!empty($result)) {
+              session_start();
+              $_SESSION['login'] = true;
+
+              header("Location:mypage.php");
+          } else {
+              $err_msg['common'] = MSG02;
+          }
+      }
+  }
+
 ?>
 <!DOCTYPE html>
 <html lang="ja">
@@ -27,15 +72,14 @@
       <section class="form-container">
         <h1 class="form-title">ログイン</h1>
         <form action="" method="post" class="form">
+        <span class="area-msg"><?php if(!empty($err_msg['common'])) echo $err_msg['common']; ?></span>
           <label>
             メールアドレス
-            <input type="text" name="email" placeholder="Email" value="">
-            <span class="area-msg"></span>
+            <input type="text" name="email" placeholder="Email" value="<?php if(!empty($_POST['email'])) echo $_POST['email']; ?>">
           </label>
           <label>
             パスワード
             <input type="password" name="pass" placeholder="パスワード">
-            <span class="area-msg"></span>
           </label>
           <button type="submit" class="btn">ログイン</button>
         </form>
